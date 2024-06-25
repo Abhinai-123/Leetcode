c607ef58e37d3ee8a05d78e5a035081e80e2c91b
Abhinai-123 <101736261+Abhinai-123@users.noreply.github.com>
2024-06-25 19:54:58 +0530

    Time: 30 ms (89.78%), Space: 16.4 MB (74.62%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -7,20 +7,13 @@
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
         def solve(n,s):
-            if n:
-                print("n.val",n.val)
-                print("sum",s)
+ 
             if not n:
-                print("from retrun",s)
                 return s
-            print("right")
             r=solve(n.right,s)
             n.val=n.val+r
             s=n.val
-            print("left")
             l=solve(n.left,s)
-            print(f"n.val{n.val}")
-            print(s)
             return l
         solve(root,0)
         return root
